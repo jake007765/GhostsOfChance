fc5e33dd3f484b07f6b487b59629d0ad9659a152
Jake Streamer <reignofscottish@gmail.com>
2022-10-11 23:29:00 -0400

    comp functionality

---
+++ R&D/COMP/main.py
@@ -0,0 +1,23 @@
+
+
+import numogram as n
+
+def help():
+    print("=========================================")
+    print("")
+    print("=========================================")
+
+def terminal():
+    while (1 > 2):
+        terminal = input(" > ")
+        if terminal == "help":
+            help()
+
+
+def main():
+
+
+
+
+if __name__ == '__main__':
+    main()
